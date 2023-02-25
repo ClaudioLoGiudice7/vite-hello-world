f4890afff6fc3d743df412043afc9059ef4861a3
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-02-25 17:57:20 +0100

    end

---
+++ src/components/AppHero.vue
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+
+
+<template>
+    <div class="jumbo"></div>
+</template>
+
+
+
+<style scoped>
+.jumbo {
+    height: 300px;
+    width: 300;
+    background-color: yellowgreen;
+}
+</style>
